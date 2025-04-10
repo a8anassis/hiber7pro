de3fd461e6cd31b94e3dc8cb604d10a657ec1dc8
Th. Androutsos <a8anassis@gmail.com>
2025-04-10 20:53:32 +0300

    feat: App with hibernate table creation and CRUD was added

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,10 +1,32 @@
 package gr.aueb.cf;
 
-/**
- * Hello world!
- */
+import gr.aueb.cf.model.Course;
+import gr.aueb.cf.model.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class App {
+   private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school7PU");
+   private final static EntityManager em = emf.createEntityManager();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+//        Teacher teacher = new Teacher(null, "Alice", "W.", null);
+//        Course java = new Course(null, "Java", null);
+//        teacher.addCourse(java);
+
+        em.getTransaction().begin();
+
+        Teacher alice = em.find(Teacher.class, 1L);
+        alice.setLastname("Wonderland");
+        em.merge(alice);
+
+//        em.persist(teacher);
+//        em.persist(java);
+
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
     }
 }
