7e632dd19692fc0a185cbe59c1425981ddaf11f8
Th. Androutsos <a8anassis@gmail.com>
2025-04-25 20:02:15 +0300

    feat: JPQL queries added

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -5,6 +5,9 @@ import gr.aueb.cf.model.Teacher;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class App {
    private final static EntityManagerFactory emf = Persistence.createEntityManagerFactory("school7PU");
@@ -17,8 +20,8 @@ public class App {
 
         em.getTransaction().begin();
 
-        Course course = em.find(Course.class, 2L);
-        em.remove(course);
+//        Course course = em.find(Course.class, 2L);
+//        em.remove(course);
 
 //        Teacher alice = em.find(Teacher.class, 1L);
 //        Course databases = new Course(null, "Databases", null);
@@ -31,6 +34,37 @@ public class App {
 //        em.persist(teacher);
 //        em.persist(java);
 
+        // Select όλους τους teachers
+//        String sql = "SELECT t FROM Teacher t";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
+
+        // Select all courses
+//        String sql = "SELECT c FROM Course c";
+//        List<Course> courses = em.createQuery(sql, Course.class).getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select courses που διδάσκει ο Μ. Καραμπάτσης
+//        String sql = "SELECT c FROM Course c WHERE c.teacher.firstname = :firstname";   // SQL Injection attack and alias
+//        TypedQuery<Course> query = em.createQuery(sql, Course.class);
+//        query.setParameter("firstname", "Μάρκος");
+//        List<Course> courses = query.getResultList();
+
+//        List<Course> courses = em.createQuery(sql, Course.class)
+//                .setParameter("firstname", "Μάρκος")
+//                .getResultList();
+//        courses.forEach(System.out::println);
+
+        // Select teachers και τίτλους courses που διδάσκουν οι teachers
+        String sql = "SELECT t, c.title FROM Teacher t JOIN t.courses c";   // INNER JOIN
+        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+        List<Object[]> results = query.getResultList();
+        for (Object[] result : results) {
+            Teacher teacher = (Teacher) result[0];
+            String courseTitle = (String) result[1];
+            System.out.println("Teacher: " + teacher.getLastname() + ", Course: " + courseTitle);
+        }
 
 
         em.getTransaction().commit();
